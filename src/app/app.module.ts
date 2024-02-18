2dc0d8cf98ea1cd53beb2f0e9e934329b533542a
achucarrob <achucarroclaudia@gmail.com>
2024-02-18 15:25:27 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './components/layout/layout.module';
+import { ListComponent } from './components/productos/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
