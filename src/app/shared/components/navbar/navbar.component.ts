03877c732d7eac3cf7ddbde3186270b495e3890a
achucarrob <achucarroclaudia@gmail.com>
2024-02-19 15:36:28 -0300

    Recibir cart de componente padre a hijo

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+  @Input() cart: any[] = [];
+  @Input() mostrarBadge: boolean = false;
 
+  if (cart = !null){
+    this.mostrarBadge = true
+  }
 }
