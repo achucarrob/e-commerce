2010af05496541eff96fdb012138ee0a90ce254f
achucarrob <achucarroclaudia@gmail.com>
2024-02-19 15:35:49 -0300

    Mejora de respuesta Api

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -9,18 +9,23 @@ export class LayoutComponent {
   // @Input({required: true}) price:number = 0;
   // @Input({required: true}) title:string = '';
 
+  cart: any[] = [];
+
   apiResponse = {
     products: [
-      { title: "Ron", price: 2500000 },
-      { title: "Ron", price: 3000000 },
-      { title: "Ron", price: 785000 },
-      { title: "Ron", price: 1200000 },
-      { title: "Ron", price: 6666666 },
-      { title: "Ron", price: 9999999}
+      { id: 1, title: "Ron", price: 2500000, img: "https://www.maahsa.com.py/storage/sku/don-onofre-bebidas-alimentos-ron-profesional-1-1-1684509239.png" },
+      { id: 2, title: "Ron", price: 3000000, img: "https://www.maahsa.com.py/storage/sku/don-onofre-bebidas-alimentos-ron-profesional-1-1-1684509239.png" },
+      { id: 3, title: "Ron", price: 785000, img: "https://www.maahsa.com.py/storage/sku/don-onofre-bebidas-alimentos-ron-profesional-1-1-1684509239.png" },
+      { id: 4, title: "Ron", price: 1200000, img: "https://www.maahsa.com.py/storage/sku/don-onofre-bebidas-alimentos-ron-profesional-1-1-1684509239.png" },
+      { id: 5, title: "Ron", price: 6666666, img: "https://www.maahsa.com.py/storage/sku/don-onofre-bebidas-alimentos-ron-profesional-1-1-1684509239.png" },
+      { id: 6, title: "Ron", price: 9999999, img: "https://www.maahsa.com.py/storage/sku/don-onofre-bebidas-alimentos-ron-profesional-1-1-1684509239.png" }
     ]
   };
 
-  addToCart(title: string, price: number){
-    console.log("Titulo", title, "Precio", price)
+
+  addToCart(title: string, price: number, img: string){
+    this.cart.push({ title: title, price: price, img: img });
+    // console.log("Carrito", this.cart)
   }
+
 }
